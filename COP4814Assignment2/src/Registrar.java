d1d5416b555adeeec1747f3088d6247950a4a0b0
Rob <Rob@Rob-PC>
2014-02-18 18:58:36 -0500

    update

--- COP4814Assignment2/src/Registrar.java
+++ COP4814Assignment2/src/Registrar.java
@@ -2,7 +2,9 @@
 public class Registrar {
 
 
-	public boolean canRegister(Status status, YearLevel year){
+	public boolean canRegister(Status status, YearLevel year, double avg, int credit){
+		
+		
 		return false;
 	}
 	
