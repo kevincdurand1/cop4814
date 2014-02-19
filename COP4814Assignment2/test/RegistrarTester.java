fc4c3458dfe9c8687abb8ec5e00efb35b5dc525a
Leo <lmene024@fiu.edu>
2014-02-18 19:45:08 -0500

    Ready to start testing

--- COP4814Assignment2/test/RegistrarTester.java
+++ COP4814Assignment2/test/RegistrarTester.java
@@ -1,11 +1,20 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 
 public class RegistrarTester{
+	Registrar registrar = new Registrar();
 
 	@Test
-	void test(){
-		Registrar x = new Registrar();
+	public void testProbLowerTrue(){
+		assertTrue("Staus Probation Year Lower with 5 credits", 
+				   registrar.canRegister(Status.Probation, YearLevel.LowerDivision, 0.0, 5));
 	}
 
+	@Test
+	public void testProbLowerFalse(){
+		assertFalse("Staus Probation Year Lower with 5 credits", 
+				   registrar.canRegister(Status.Probation, YearLevel.LowerDivision, 0.0, 8));
+	}
 }
