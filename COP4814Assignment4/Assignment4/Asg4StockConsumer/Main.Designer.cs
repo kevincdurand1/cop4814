3569d436a5c5967cb24488cf9faa0dff536cfe36
lmene024 <lmene024@fiu.edu>
2014-04-03 19:41:17 -0400

    created consumer project initial setup

---
+++ COP4814Assignment4/Assignment4/Asg4StockConsumer/Main.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Asg4StockConsumer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.chtDisplay = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chtDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStart.Location = new System.Drawing.Point(124, 20);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(105, 20);
+            this.dtpStart.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(17, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Start Date";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnShow);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dtpEnd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtpStart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(784, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(17, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select End Date";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEnd.Location = new System.Drawing.Point(124, 62);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(105, 20);
+            this.dtpEnd.TabIndex = 2;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(248, 62);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(95, 23);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "Display Chart";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // chtDisplay
+            // 
+            this.chtDisplay.Location = new System.Drawing.Point(12, 114);
+            this.chtDisplay.Name = "chtDisplay";
+            series2.Name = "Series1";
+            this.chtDisplay.Series.Add(series2);
+            this.chtDisplay.Size = new System.Drawing.Size(784, 369);
+            this.chtDisplay.TabIndex = 3;
+            this.chtDisplay.Text = "Stock Prices";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 495);
+            this.Controls.Add(this.chtDisplay);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Main";
+            this.Text = "Stock Price Display";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chtDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtDisplay;
+    }
+}
+
--- COP4814Assignment4/Assignment4/Asg4WebService/App_Code/StockReader.cs
+++ COP4814Assignment4/Assignment4/Asg4WebService/App_Code/StockReader.cs
@@ -8,68 +8,67 @@ using System.Web;
 /// </summary>
 public class StockReader
 {
-  		public static List<StockData> Read(string strPath)
-		{
-            List<StockData> lstStockData;
-			try
-			{
-				System.Xml.XmlTextReader reader = new System.Xml.XmlTextReader(strPath);
-				reader.Read();
-                lstStockData = new List<StockData>();
+    public static List<StockData> Read(string strPath)
+    {
+        List<StockData> lstStockData;
+        try
+        {
+            System.Xml.XmlTextReader reader = new System.Xml.XmlTextReader(strPath);
+            reader.Read();
+            lstStockData = new List<StockData>();
 
-				while (reader.Read())
-				{
-					reader.MoveToContent();
-					if (reader.NodeType == System.Xml.XmlNodeType.Element)
-					{
-						if (reader.Name == "Row")
-						{
-							StockData S = readClosingPrices(reader);
-							lstStockData.Add(S);
-						}
-					}
-				}
-				return lstStockData;
-			}
-			catch (System.IO.FileNotFoundException)
-			{
-				return null;
-			}
-		}
+            while (reader.Read())
+            {
+                reader.MoveToContent();
+                if (reader.NodeType == System.Xml.XmlNodeType.Element)
+                {
+                    if (reader.Name == "Row")
+                    {
+                        StockData S = readClosingPrices(reader);
+                        lstStockData.Add(S);
+                    }
+                }
+            }
+            return lstStockData;
+        }
+        catch (System.IO.FileNotFoundException)
+        {
+            return null;
+        }
+    }
 
-		static string nextValue(System.Xml.XmlTextReader reader)
-		{
-			while (reader.Read())
-			{
-				if (reader.NodeType == System.Xml.XmlNodeType.Text)
-					return reader.Value;
-			}
-			return null;
-		}
+    static string nextValue(System.Xml.XmlTextReader reader)
+    {
+        while (reader.Read())
+        {
+            if (reader.NodeType == System.Xml.XmlNodeType.Text)
+                return reader.Value;
+        }
+        return null;
+    }
 
-		static StockData readClosingPrices(System.Xml.XmlTextReader reader)
-		{
-			// Excel dates are encoded as integers, where 1/1/1900 = 1. There is a bug
-			// in their date calculations because they consider 1900 to be a leap year. This
-			// bug was never corrected because it first appeared in Lotus 1-2-3 and Microsoft
-			// wanted the two products to be compatible.
+    static StockData readClosingPrices(System.Xml.XmlTextReader reader)
+    {
+        // Excel dates are encoded as integers, where 1/1/1900 = 1. There is a bug
+        // in their date calculations because they consider 1900 to be a leap year. This
+        // bug was never corrected because it first appeared in Lotus 1-2-3 and Microsoft
+        // wanted the two products to be compatible.
 
-			int offset = 2;     
-			int excelDays = Convert.ToInt32(nextValue(reader)) - offset;
-			DateTime start = new DateTime(1900, 1, 1);
-			TimeSpan ts = new TimeSpan(excelDays,0,0,0);
-			DateTime D = start.Add(ts);
+        int offset = 2;
+        int excelDays = Convert.ToInt32(nextValue(reader)) - offset;
+        DateTime start = new DateTime(1900, 1, 1);
+        TimeSpan ts = new TimeSpan(excelDays, 0, 0, 0);
+        DateTime D = start.Add(ts);
 
-			double open = Convert.ToDouble(nextValue(reader));
-			double high = Convert.ToDouble(nextValue(reader));
-			double low = Convert.ToDouble(nextValue(reader));
-			double close = Convert.ToDouble(nextValue(reader));
-			double volume = Convert.ToDouble(nextValue(reader));
+        double open = Convert.ToDouble(nextValue(reader));
+        double high = Convert.ToDouble(nextValue(reader));
+        double low = Convert.ToDouble(nextValue(reader));
+        double close = Convert.ToDouble(nextValue(reader));
+        double volume = Convert.ToDouble(nextValue(reader));
 
-			StockData S = new StockData(D, close);
-			return S;
-		}
-
-	}
+        StockData S = new StockData(D, close);
+        return S;
+    }
 
 }
+
